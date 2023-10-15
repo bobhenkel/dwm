c030724da71197223c9a874a77251fae7ed86d3b
bobhenkel <bob.henkel@gmail.com>
2023-10-15 16:06:23 -0500

    Make selected tag text red and bar text red

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#222222"; //Status bar background color on the far right of bar
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#bbbbbb"; //Tag bar text
+static const char col_gray4[]       = "#e57474"; //"#eeeeee"; //Bar window text color
+static const char col_cyan[]        = "#67b0e8";  //"#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
